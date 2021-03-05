b746a8580b66e1db73df63462bc718c3de8f72b2
Nusrat-Jahan <jahannusrat735@gmail.com>
2021-03-05 15:11:34 +0100

    Used 'forEach' loop Instead of for loop

--- js/scripts.js
+++ js/scripts.js
@@ -24,10 +24,17 @@ let pokemonList = [
 // used for loop to print pokemon name and height
 //used template literals `${}` for adding height
 
-for(let i = 0; i < pokemonList.length; i++){
-  if(pokemonList[i].height > 0.7){
-    document.write('<p>'+ pokemonList[i].name + ` (height: ${pokemonList[i].height}) - Wow, that's big!` + '<p>');
+// for(let i = 0; i < pokemonList.length; i++){
+//   if(pokemonList[i].height > 0.7){
+//     document.write('<p>'+ pokemonList[i].name + ` (height: ${pokemonList[i].height}) - Wow, that's big!` + '<p>');
+//   }else{
+//   document.write('<p>' + pokemonList[i].name + ` (height: ${pokemonList[i].height})` + '</p>' )
+//  }
+// }
+pokemonList.forEach(function(item){
+  if(item.height > 0.7){
+    document.write(`<p> ${item.name} (height: ${item.height}) - wow that's big </p>`);
   }else{
-  document.write('<p>' + pokemonList[i].name + ` (height: ${pokemonList[i].height})` + '</p>' )
- }
-}
+    document.write(`<p> ${item.name} (height: ${item.height}) </p>`);
+  }
+});
